b2b594a1c501ffd3732c08cce511b63a7ec3f95b
peernohell <peernohell@gmail.com>
2014-12-10 09:31:20 +0100

    update c3 to version 0.4.7

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'peernohell:c3',
-  summary: "A Meteor port for the C3 charting library based on D3. Clone of TackMobile/meteor-c3.js",
-  version: "1.0.0",
+  summary: "A Meteor port for the C3 (0.4.7) charting library based on D3. Clone of TackMobile/meteor-c3.js",
+  version: "1.1.0",
   git: "https://github.com/peernohell/meteor-c3.js.git"
 });
 
