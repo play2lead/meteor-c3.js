2e5a8605a638bd85fa8378c78dd5c1a09f4e0a8b
peernohell <peernohell@gmail.com>
2015-04-04 10:19:23 +0200

    update package version and remove commented code

--- package.js
+++ package.js
@@ -1,14 +1,13 @@
 Package.describe({
   name: 'peernohell:c3',
   summary: "C3 (0.4.10) charting library based on D3.",
-  version: "1.1.2",
+  version: "1.1.3",
   git: "https://github.com/peernohell/meteor-c3.js.git"
 });
 
 Package.on_use(function (api, where) {
   api.versionsFrom("METEOR@0.9.0");
   api.use("sergeyt:d3@3.4.1", 'client');
-  //api.use("d3js:d3@3.4.13", 'client');
   api.add_files([
     'c3/c3.min.js',
     'c3/c3.css'
