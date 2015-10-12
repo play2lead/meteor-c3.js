6befcdc3b0deb2718fa058beff91301c06e4a653
Maxim Filimonov <tpaktopsp@gmail.com>
2015-10-12 12:16:20 +0800

    switch to using custom c3

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: 'peernohell:c3',
+  name: 'play2lead:c3',
   summary: "C3 (0.4.10) charting library based on D3.",
   version: "1.1.3",
   git: "https://github.com/peernohell/meteor-c3.js.git"
@@ -9,7 +9,7 @@ Package.on_use(function (api, where) {
   api.versionsFrom("METEOR@0.9.0");
   api.use("sergeyt:d3@3.4.1", 'client');
   api.add_files([
-    'c3/c3.min.js',
+    'c3/c3.js',
     'c3/c3.css'
   ], 'client');
 });
